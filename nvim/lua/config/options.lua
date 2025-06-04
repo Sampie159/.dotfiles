c0075974edbe253fad130d0cf765a95569ad73a9
Sampie159 <38163547+Sampie159@users.noreply.github.com>
2025-06-04 01:40:56 -0300

    buh

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -6,3 +6,19 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.g.autoformat = false
+
+-- LSP Configs
+
+vim.lsp.config('ols', {
+    cmd = { "ols", "--stdio" },
+    filetypes = { "odin" },
+    root_markers = { '.git', 'odinfmt.json', 'ols.json' },
+})
+vim.lsp.enable('ols')
+
+vim.lsp.config('zls', {
+    cmd = { 'zls' },
+    filetypes = { 'zig', 'zir' },
+    root_markers = { '.git', 'build.zig', 'zls.json' },
+})
+vim.lsp.enable('zls')
