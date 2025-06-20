d731127e0f39b1fab3425b9ac97a10d3e81b731a
Sampie159 <38163547+Sampie159@users.noreply.github.com>
2025-06-20 19:06:54 -0300

    buh

---
+++ nvim/lua/core/functions.lua
@@ -0,0 +1,77 @@
+local function label(args)
+    local lbl = args.args
+    local buf = vim.api.nvim_get_current_buf()
+    local line = vim.api.nvim_win_get_cursor(0)[1] - 1
+    local str = "/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */"
+    local lbl_length = string.len(lbl)
+    local padding = (75 - lbl_length) / 2
+    local extra = lbl_length % 2 == 0 and 1 or 0
+    local str2 = "/*" ..
+        string.rep(" ", padding) ..
+        string.upper(lbl) .. string.rep(" ", padding + extra) .. "*/"
+
+    vim.api.nvim_buf_set_lines(buf, line, line, false, { str, str2, str })
+end
+
+local function vcvars()
+    local buf = vim.api.nvim_get_current_buf()
+    local line = vim.api.nvim_win_get_cursor(0)[1] - 1
+    local str = [[
+set msvc22p=C:\Program Files\Microsoft Visual Studio\2022\Professional
+set msvc22c=C:\Program Files\Microsoft Visual Studio\2022\Community
+set msvc19p=C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional
+set msvc19c=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community
+set msvc17p=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional
+set msvc17c=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community
+set msvc14=C:\Program Files (x86)\Microsoft Visual Studio 14.0
+set msvc13=C:\Program Files (x86)\Microsoft Visual Studio 13.0
+set msvc12=C:\Program Files (x86)\Microsoft Visual Studio 12.0
+set msvc11=C:\Program Files (x86)\Microsoft Visual Studio 11.0
+set msvc10=C:\Program Files (x86)\Microsoft Visual Studio 10.0
+if exist "%msvc22p%" (
+    call "%msvc22p%\VC\Auxiliary\Build\vcvarsall.bat" x64
+) else if exist "%msvc22c%" (
+    call "%msvc22c%\VC\Auxiliary\Build\vcvarsall.bat" x64
+) else if exist "%msvc19p%" (
+    call "%msvc19p%\VC\Auxiliary\Build\vcvarsall.bat" x64
+) else if exist "%msvc19c%" (
+    call "%msvc19c%\VC\Auxiliary\Build\vcvarsall.bat" x64
+) else if exist "%msvc17p%" (
+    call "%msvc17p%\VC\Auxiliary\Build\vcvarsall.bat" x64
+) else if exist "%msvc17c%" (
+    call "%msvc17c%\VC\Auxiliary\Build\vcvarsall.bat" x64
+) else if exist "%msvc14%" (
+    call "%msvc14%\VC\vcvarsall.bat" x64
+) else if exist "%msvc13%" (
+    call "%msvc13%\VC\vcvarsall.bat" x64
+) else if exist "%msvc12%" (
+    call "%msvc12%\VC\vcvarsall.bat" x64
+) else if exist "%msvc11%" (
+    call "%msvc11%\VC\vcvarsall.bat" x64
+) else if exist "%msvc10%" (
+    call "%msvc10%\VC\vcvarsall.bat" x64
+)
+]]
+    local lines = {}
+    for s in str:gmatch("[^\r\n]+") do
+        table.insert(lines, s)
+    end
+
+    vim.api.nvim_buf_set_text(buf, line, 0, line, 0, lines)
+end
+
+function begin_command(cmd)
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>:" .. cmd .. " ", true, false, true), 'n', false)
+end
+
+function search_copen(word)
+    vim.cmd('silent grep ' .. word.args)
+    vim.defer_fn(function()
+        vim.cmd('copen')
+    end, 50)
+end
+
+vim.api.nvim_create_user_command('Label', label, { nargs = "*" })
+vim.api.nvim_create_user_command('Vcvars', vcvars, {})
+vim.api.nvim_create_user_command('Search', search_copen, { nargs = 1 })
+vim.keymap.set('i', '<M-l>', '<CMD>lua begin_command("Label")<CR>', { noremap = true, silent = true })
---
+++ nvim/lua/core/plugin_config/lsp_config.lua
@@ -0,0 +1,92 @@
+local lsp = require('lspconfig')
+
+local on_attach = function(client, bufnr)
+    -- client.server_capabilities.semanticTokensProvider = nil
+    local nmap = function(keys, func, desc)
+        if desc then
+            desc = 'LSP: ' .. desc
+        end
+
+        vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+    end
+    -- Lesser used LSP functionality
+    nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+    nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+    nmap('<leader>wl', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, '[W]orkspace [L]ist Folders')
+
+    -- Create a command `:Format` local to the LSP buffer
+    vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+        vim.lsp.buf.format()
+    end, { desc = 'Format current buffer with LSP' })
+end
+
+vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename)
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action)
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
+vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references)
+vim.keymap.set('n', 'gI', vim.lsp.buf.implementation)
+-- vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition)
+-- vim.keymap.set('n', '<leader>ds', require('telescope.builtin').lsp_document_symbols)
+-- vim.keymap.set('n', '<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols)
+vim.keymap.set('n', 'gD', vim.lsp.buf.declaration)
+vim.keymap.set("n", "<leader>F", function()
+    vim.lsp.buf.format()
+    vim.api.nvim_command('write')
+end)
+
+-- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+local servers = {
+    lua_ls = {
+        Lua = {
+            workspace = { checkThirdParty = false },
+            telemetry = { enable = false },
+        },
+    },
+}
+
+vim.lsp.enable('gopls')
+
+-- C/C++ lsp config
+lsp.clangd.setup {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    cmd = { 'clangd', '--background-index', '--clang-tidy', '--completion-style=bundled', '--header-insertion=never', '--header-insertion-decorators=0' },
+    filetypes = { 'c', 'cpp' },
+    init_options = {
+        clangdFileStatus = true,
+        usePlaceholders = true,
+        completeUnimported = true,
+        semanticHighlighting = false,
+    },
+    root_dir = lsp.util.root_pattern('.clangd', '.clang-format', '.clang-tidy', '.clang=format', 'configure.ac',
+        'compile_commands.json',
+        'compile_flags.txt', '.git'
+    ),
+}
+
+vim.lsp.enable('rust_analyzer')
+
+vim.lsp.enable("zls")
+
+vim.lsp.enable('glsl_analyzer')
+
+vim.lsp.enable("ols")
+
+vim.lsp.enable("omnisharp")
+
+vim.lsp.enable('neocmake')
+
+-- Setup mason so it can manage external tooling
+require('mason').setup()
+
+-- Ensure the servers above are installed
+local mason_lspconfig = require 'mason-lspconfig'
+
+mason_lspconfig.setup {
+    ensure_installed = vim.tbl_keys(servers),
+}
