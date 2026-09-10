6b8334d4fff847d34b3887a106c3fe6d461771fb
Sampie159 <38163547+Sampie159@users.noreply.github.com>
2026-09-10 00:08:35 -0300

    configuration: console.keyMap us

--- configuration.nix
+++ configuration.nix
@@ -26,7 +26,7 @@
 
     time.timeZone = "America/Sao_Paulo";
     i18n.defaultLocale = "en_US.UTF-8";
-    console.keyMap = "br-abnt2";
+    console.keyMap = "us";
 
     services = {
         displayManager.ly = {
